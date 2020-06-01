68cb5b708b0f9851ab162147c863c3a641ee85c8
Matthew-Cooke289 <66196886+Matthew-Cooke289@users.noreply.github.com>
2020-06-01 23:10:52 +0800

    Update top.v

--- Ex2/top.v
+++ Ex2/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #2 
-// Student Name:
-// Date: 
+// Student Name: Matthew Cooke
+// Date: 1/6/2020
 //
 //  Description: In this exercise, you need to design a multiplexer, where the  
 //  output acts according to the following truth table:
@@ -21,12 +21,26 @@
 `timescale 1ns / 100ps
 
 module mux(
-    //Todo: define inputs here
+    input a,
+    input b,
+    input sel,
+    output out
     );
-    
-    //Todo: define registers and wires here
+	
+	parameter CLK_PERIOD = 10;
+    	reg  clk, out;
 
+	initial begin 
+	  clk=0;   
+	forever
+	#(CLK_PERIOD/2) 
+	clk=~clk;
+	end
 
-    //Todo: define your logic here                 
+    always @(posedge clk)
+	if(sel)
+	out = b;
+	else
+	out = a ;               
       
 endmodule
