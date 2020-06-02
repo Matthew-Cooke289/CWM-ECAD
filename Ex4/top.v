1e3dbb559fd0b6c9b33825047cdf640a95604ab6
Matthew-Cooke289 <66196886+Matthew-Cooke289@users.noreply.github.com>
2020-06-02 17:56:47 +0800

    Update top.v

--- Ex4/top.v
+++ Ex4/top.v
@@ -15,4 +15,39 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module dice(
+     
+	input rst, button,
+	output reg [2:0] throw 
+    );
+    
+    parameter CLK_PERIOD = 10;
+	reg clk
+    	
+
+	initial begin 
+	  clk=0;   
+	forever
+	#(CLK_PERIOD/2) 
+	clk=~clk;
+	end
+
+    always @(posedge clk)
+	if(rst)
+	throw = 3'b000;
+	else
+	if(!button && (throw == 3'b000 || 3'b111))
+	throw = 3'b001;
+	else
+	if(button && (throw !== 3'b111 || 3'b110))
+	throw = throw + 3'b001;
+	else
+	if(button && (throw == 3'b111 || 3'b110))
+	throw = 3'b001;
+
+
+                    
+      
+endmodule
