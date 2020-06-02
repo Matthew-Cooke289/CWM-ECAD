c6b835e338d1fd469f326f7a5e2d25af759b908d
Matthew-Cooke289 <66196886+Matthew-Cooke289@users.noreply.github.com>
2020-06-02 15:47:24 +0800

    Update top_tb.v

--- Ex3/top_tb.v
+++ Ex3/top_tb.v
@@ -11,18 +11,73 @@
 `timescale 1ns / 100ps
 
 module top_tb(
+
     );
     
 //Todo: Parameters
+parameter CLK_PERIOD = 10;
 
 //Todo: Regitsers and wires
+	reg clk;
+    reg rst;
+    reg err;
+	reg enable;
+	reg direction;
+    wire [7:0] counter_out;
 
-//Todo: Clock generation
+//Clock generation
+    initial
+    begin
+       clk = 1'b0;
+       forever
+         #(CLK_PERIOD/2) clk=~clk;
+     end
 
 //Todo: User logic
+	initial begin
+   
+    assign 
+	rst = 1;
+    #10
+    rst = 0;
+    enable = 1;
+    direction = 0;
+    #640;
+    enable = 0;
+    #20;
+    enable = 1;
+    direction = 1;
+    #440;
+
+    end
+
     
 //Todo: Finish test, check for success
+	initial 
+	begin
+
+	if(counter_out != 8'b00000000)
+	begin
+	err <= 1;
+	end
+
+
+	if (err==1)
+	begin
+	$display("***TEST FAILED :( Final_Counter_Value ==\d, ***", counter_out);
+	end
+	else begin
+	$display("***TEST PASSED! :) ***");
+	end
+
+	end
 
 //Todo: Instantiate counter module
- 
-endmodule 
+ counter top (
+     .rst (rst),
+     .enable (enable),
+	.direction (direction),
+     .counter_out (counter_out)
+     );
+
+endmodule  
