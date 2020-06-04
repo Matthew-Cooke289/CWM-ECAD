b4d974108a748cee130e93cb29493bdf684310d1
Matthew-Cooke289 <66196886+Matthew-Cooke289@users.noreply.github.com>
2020-06-04 18:07:41 +0800

    Update top_tb.v

--- Ex5/top_tb.v
+++ Ex5/top_tb.v
@@ -6,54 +6,41 @@
 // Description: A testbench module to test Ex5 - Traffic Lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+
 `timescale 1ns / 100ps
 
-module top_tb(
-    );
-    
-//Todo: Parameters
+module lights(
+	input wire clk,
+	output reg red, amber, green,
+	output reg [2:0] RAG
+);
+                  
+    //Todo: add registers and wires, if needed
 
-//Todo: Regitsers and wires
-	reg clk, err;
-	wire green, amber, red, 
-	wire [2:0] RAG;
+	
+	
+	
 
-// Clock generation
-initial
-    begin
-       clk = 1'b0;
-       forever
-         #5 
-	clk=~clk;
-     end
+    //Todo: add user logic
 
-//Todo: User logic
-
-	always @(negedge clk)
-	begin
+	always @(posedge clk)     
+begin
 	if(RAG == 3'b000 | 3'b101 | 3'b011 | 3'b111)
-	begin 
-	err <= 1;
-	end
+	RAG <= 3'b100;
+	else
+	if(RAG == 3'b100)
+	RAG <= 3'b110;
+	else
+	if(RAG == 3'b110)
+	RAG <= 3'b001;
+	else
+	if(RAG == 3'b001)
+	RAG <= 3'b010;
+	else
+	if(RAG == 3'b010)
+	RAG <= 3'b100;
+	else
+	RAG <= 3'b100;
+end
 
-	if (err==1)
-	begin
-	$display("***TEST FAILED :( invalid state Final_Counter_Value ==%d, ***", RAG);
-	end
-	else 
-	begin
-	$display("***TEST PASSED! reset works :) ***");
-	end
-	end
-    
-//Todo: Finish test, check for success
-
-//Todo: Instantiate counter module
- dice light ( 
-	.clk (clk),
-    	.red (red),
-	.amber (amber),
-	.green (green),
-	.RAG (RAG)
-     );
-endmodule 
+endmodule
